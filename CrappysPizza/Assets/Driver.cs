17a70b2b681413168e56c47495bef123d037e9cf
cwilliams1981 <chuckwilliams1981@gmail.com>
2021-10-13 23:41:11 -0400

    CrappysPizza 00.00.00.04 - Introducing Variables

--- CrappysPizza/Assets/Driver.cs
+++ CrappysPizza/Assets/Driver.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    float turnSpeed = 1f;
+    float moveSpeed = 0.01f;
+
     void Start()
     {
-
+        
     }
 
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.01f, 0);
+        transform.Rotate(0, 0, turnSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
