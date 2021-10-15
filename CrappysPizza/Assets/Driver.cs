54e8cd5a2dff2a8fb49ecf1bc97497aca790ef85
cwilliams1981 <chuckwilliams1981@gmail.com>
2021-10-14 20:59:08 -0400

    CrappysPizza 00.00.00.07 - Using Time.deltaTime()

--- CrappysPizza/Assets/Driver.cs
+++ CrappysPizza/Assets/Driver.cs
@@ -14,8 +14,8 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        float turnAmount = Input.GetAxis("Horizontal") * turnSpeed;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        float turnAmount = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -turnAmount);
         transform.Translate(0, moveAmount, 0);
     }
