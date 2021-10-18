406c52b4d911066ef029311c12b14f7211534ab4
cwilliams1981 <chuckwilliams1981@gmail.com>
2021-10-17 20:16:37 -0400

    CrappysPizza 00.00.00.17 - How To Use GetComponent

--- CrappysPizza/Assets/Delivery.cs
+++ CrappysPizza/Assets/Delivery.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
     [SerializeField] float destroyDelay = 0f;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     bool hasPackage = false;
     private void OnCollisionEnter2D(Collision2D collision)
@@ -18,12 +28,14 @@ public class Delivery : MonoBehaviour
             Debug.Log("Package picked up!");
             hasPackage = true;
             Destroy(other.gameObject, destroyDelay);
+            spriteRenderer.color = hasPackageColor;
         }
         else if ((other.tag == "Customer") && (hasPackage))
         {
             Debug.Log("Package delivered to customer!");
             hasPackage = false;
             Destroy(other.gameObject, destroyDelay);
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
